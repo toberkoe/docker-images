e96cd9b08d5414336f92d812a567abb65914f93d
t.bertram-koehler <t.bertramkoehler@gmail.com>
2018-04-05 13:25:47 +0200

    make use of docker templating

---
+++ wildfly/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+update() {
+sedStr="
+  s!%%WILDFLY_VERSION%%!$version!g;
+"
+
+sed -r "$sedStr" $1
+}
+
+versions=(11.0.0.Final 12.0.0.Final)
+for version in ${versions[*]}; do
+  mkdir $version
+  update Dockerfile.template > $version/Dockerfile
+done
