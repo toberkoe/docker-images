ddf293768c338fbf8b5af3d9d51ec4dd21ea6747
t.bertram-koehler <t.bertram-koehler@euroimmun.de>
2018-07-31 13:56:01 +0200

    added toberkoe/wildfly:13.0.0.Final

--- wildfly/update.sh
+++ wildfly/update.sh
@@ -8,7 +8,7 @@ sedStr="
 sed -r "$sedStr" $1
 }
 
-versions=(11.0.0.Final 12.0.0.Final)
+versions=(11.0.0.Final 12.0.0.Final 13.0.0.Final)
 for version in ${versions[*]}; do
   mkdir $version
   update Dockerfile.template > $version/Dockerfile
